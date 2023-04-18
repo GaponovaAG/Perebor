aa550e3cee46d85a5adbe1dee2084f45a657d6a5
GaponovaAG <108420381+GaponovaAG@users.noreply.github.com>
2023-04-18 17:31:32 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Введите число от 0 до 100")
+x = int(input())
+if x % 3 == 0 and x % 5 == 0:
+    print("FizzBuzz")
+elif x % 3 == 0:
+    print("Fizz")
+elif x % 5 == 0:
+    print("Buzz")
+else:
+    print(x)
+
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
